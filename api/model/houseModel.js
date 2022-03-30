729c720b385a7588cf215c6e172c8e172e42a51b
Rishabh Domadiya <rishabh.canada5@gmail.com>
2022-03-29 22:24:15 -0300

    Manage rentals backend code added

--- api/model/houseModel.js
+++ api/model/houseModel.js
@@ -1,23 +1,21 @@
-/* 
-  authorName : Sanjuna Konda 
-  email : sn493898@dal.ca
-*/
-
-const mongoose = require("mongoose");
-
-const houseSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    address: { type: String, required: true },
-    city: { type: String, required: true },
-    rooms: { type: String, required: true },
-    bathrooms: { type: String, required: true },
-    category: { type: String, required: true },
-    peoplecount: { type: String, required: true },
-    price: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: String, required: true }
-  
-  });
-  
-  module.exports = mongoose.model("House", houseSchema);
+module.exports = mongoose => {
+  const House = mongoose.model(
+      "house",
+      mongoose.Schema(
+          {
+              title: String,
+              description: String,
+              selectedFile: String,
+              address: { street: String, province: String, city: String },
+              category: String,
+              rooms: String,
+              bathrooms: Number,
+              price: Number,
+              email: String,
+              phone: Number
+          },
+          { timestamps: true }
+      )
+  );
+  return House;
+};
