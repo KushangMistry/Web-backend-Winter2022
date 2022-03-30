1d838ed6f81c55f5de89af5541a2f5d1f8436b4d
VISHVESH BHARATBHAI NAIK <vs307949@dal.ca>
2022-03-30 18:31:16 -0300

    Added Reply Task

---
+++ api/model/threadReply.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+// const jwt = require("jsonwebtoken");
+
+const threadReply = new mongoose.Schema({
+  threadId: { type: String, required: true },
+  replyDesc: { type: String, required: true },
+  replyDate: { type: String, default: () => new Date() },
+  replyStatus: { type: Boolean, default: () => true },
+  userName: { type: String, required: true },
+
+  //   _id: mongoose.Schema.Types.ObjectId,
+});
+
+module.exports = mongoose.model("reply", threadReply);
